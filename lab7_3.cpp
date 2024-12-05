047a6f2a1a27b367de675e167d80c04dbea66ad3
MeTOR1657 <chokun246810@gmail.com>
2024-12-05 23:05:16 +0700

    Completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -12,18 +12,3 @@ char before(char x) {
     }
     return '0';
 }
-
-int main() {
-    char input;
-    cout << "Enter a capital letter (A-Z): ";
-    cin >> input;
-
-    char result = before(input);
-    if (result == '0') {
-        cout << "Invalid input! Please enter a capital letter between A and Z." << endl;
-    } else {
-        cout << "The letter before " << input << " is: " << result << endl;
-    }
-
-    return 0;
-}
